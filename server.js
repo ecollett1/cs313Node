1a68edfe3d60f14642c6064ed870e91b0a7310cd
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-21 14:05:02 -0600

    Finished the app stuff.

--- server.js
+++ server.js
@@ -11,26 +11,26 @@ let server = http.createServer(function(req, res) {
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(express.static(__dirname + '/public'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true}));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  	response.sendFile(__dirname + '/public/letter.html');
-});
+// app.get('/', function(request, response) {
+//   	response.sendFile(__dirname + '/public/letter.html');
+// });
 
-app.get('/postage', function(req, res) {
-	var weight = req.query.weight;
-	var type = req.query.letterType;
+// app.get('/postage', function(req, res) {
+// 	var weight = req.query.weight;
+// 	var type = req.query.letterType;
 
-	postage.calculateRate(weight, type, function(error, results) {
-		res.render("pages/results", results);
-	});
-});
+// 	postage.calculateRate(weight, type, function(error, results) {
+// 		res.render("pages/results", results);
+// 	});
+// });
 
 app.listen(app.get('port'), function() {
   	console.log('Node app is running on port', app.get('port'));
