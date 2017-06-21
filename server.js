b98ffb5a7a40a202b967b12465ecbaeb2d2ce71b
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-21 14:10:32 -0600

    Finished the app stuff.

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.set('port', (process.env.PORT || 5000));
 // 	});
 // });
 
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
   	console.log('Node app is running on port', app.get('port'));
 });
