699622c335d2084c13f086cb8106a772d5deea41
ecollett1 <ericmichaelcollett@gmail.com>
2017-07-10 14:13:21 -0600

    Tried to fix the server for heroku...

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/postage', function(req, res) {
 });
 
 app.get('/getUser', function(req, response){
-  response.send(row1);
+  response.send('pages/start', row1);
 	var email = req.query.username;
   console.log('Email:', email);
 	if (email) {
